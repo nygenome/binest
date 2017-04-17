95161037d070a1403b2dcdf4181959db948e63fe
Rajeeva Musunuri <rmusunuri@nygenome.org>
2017-04-17 16:54:53 -0400

    add cov scaffold

--- cov/cov.go
+++ cov/cov.go
@@ -1,4 +1,25 @@
 package cov
 
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
 // Run is the command line interface for binest cov
-func Run() {}
+func Run() {
+	bampath := flag.String("bam", "", "path to bam file")
+	excludeBed := flag.String("excludeBed", "", "path to bed file with regions to exclude")
+	flag.Parse()
+
+	if *bampath == "" {
+		fmt.Fprintln(os.Stderr, "No bam file provided!")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	EstimateCoverage(*bampath, *excludeBed)
+}
+
+// EstimateCoverage estimates the coverage excluding the regions in the bed file
+func EstimateCoverage(bampath string, excludeBed string) {}
