abde82a642ceca39ad89faaadb5e53d0138beec9
Rajeeva Musunuri <rmusunuri@nygenome.org>
2017-04-18 19:00:52 -0400

    add basic normed bin output to stdout for cov

--- cov/cov.go
+++ cov/cov.go
@@ -4,12 +4,15 @@ import (
 	"flag"
 	"fmt"
 	"os"
+
+	"github.com/biogo/hts/bam"
+
+	"github.com/omicsnut/binest"
 )
 
 // Run is the command line interface for binest cov
 func Run() {
 	bampath := flag.String("bam", "", "path to bam file")
-	excludeBed := flag.String("excludeBed", "", "path to bed file with regions to exclude")
 	flag.Parse()
 
 	if *bampath == "" {
@@ -18,8 +21,36 @@ func Run() {
 		os.Exit(1)
 	}
 
-	EstimateCoverage(*bampath, *excludeBed)
+	EstimateCoverage(*bampath)
 }
 
 // EstimateCoverage estimates the coverage excluding the regions in the bed file
-func EstimateCoverage(bampath string, excludeBed string) {}
+func EstimateCoverage(bampath string) {
+	bamFh, err := os.Open(bampath)
+	checkError(err)
+
+	bamIdxFh, err := os.Open(fmt.Sprintf("%s.bai", bampath))
+	checkError(err)
+
+	bamRdr, err := bam.NewReader(bamFh, 2)
+	checkError(err)
+
+	bamIdx, err := bam.ReadIndex(bamIdxFh)
+	checkError(err)
+
+	si, err := binest.NewSampleIndex(bamIdx, bamRdr.Header())
+	checkError(err)
+
+	bins, err := si.NormalizedBins()
+	checkError(err)
+
+	for i := 0; i < len(bins); i++ {
+		fmt.Fprintln(os.Stdout, bins[i])
+	}
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
