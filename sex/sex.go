e846240810ed59e6ba47de005a2716dedfe0b530
Rajeeva Musunuri <rmusunuri@nygenome.org>
2017-04-19 14:15:56 -0400

    add first version of sex chrom estimation

--- sex/sex.go
+++ sex/sex.go
@@ -1,4 +1,171 @@
 package sex
 
+import (
+	"flag"
+	"fmt"
+	"os"
+	"sort"
+
+	"github.com/biogo/hts/bam"
+
+	"github.com/omicsnut/binest"
+)
+
 // Run is the command line interface for binest sex
-func Run() {}
+func Run() {
+	infile := flag.String("in", "", "path to file with list of bam files")
+	outfile := flag.String("out", "", "path to output file. Default - STDOUT")
+	flag.Parse()
+
+	bampaths := make(chan string, 100)
+	results := make(chan sexEstimate, 100)
+	doneChan := make(chan bool, 1)
+
+	var outStream *os.File
+	if *outfile != "" {
+		outStream, err := os.Open(*outfile)
+		binest.CheckError(err)
+		defer outStream.Close()
+	} else {
+		outStream = os.Stdout
+	}
+
+	go EstimateSex(bampaths, results)
+	go writeResults(results, doneChan, outStream)
+
+	for _, b := range flag.Args() {
+		bampaths <- b
+	}
+
+	if *infile != "" {
+		fh, err := os.Open(*infile)
+		binest.CheckError(err)
+		binest.StreamLines(fh, bampaths)
+	}
+
+	if binest.HasStdin() {
+		binest.StreamLines(os.Stdin, bampaths)
+	}
+
+	close(bampaths)
+	<-doneChan
+}
+
+// EstimateSex estimates the sex of the samples from the BAM index
+func EstimateSex(bampaths <-chan string, results chan<- sexEstimate) {
+	for bampath := range bampaths {
+		func() {
+			bamFh, err := os.Open(bampath)
+			binest.CheckError(err)
+			defer bamFh.Close()
+
+			bamIdxFh, err := os.Open(fmt.Sprintf("%s.bai", bampath))
+			if err != nil {
+				bamIdxFh, err = os.Open(bampath[:len(bampath)-4] + ".bai")
+				binest.CheckError(err)
+			}
+			defer bamIdxFh.Close()
+
+			bamRdr, err := bam.NewReader(bamFh, 2)
+			binest.CheckError(err)
+			defer bamRdr.Close()
+
+			bamIdx, err := bam.ReadIndex(bamIdxFh)
+			binest.CheckError(err)
+
+			si, err := binest.NewSampleIndex(bamIdx, bamRdr.Header())
+			binest.CheckError(err)
+
+			normedData, err := si.NormalizedBins()
+			binest.CheckError(err)
+
+			estimate := getSexEstimate(normedData)
+			estimate.sampleName = si.Name
+			results <- estimate
+		}()
+	}
+
+	close(results)
+}
+
+// getSexEstimate gets the sexEstimate from the normalized bin data
+func getSexEstimate(d binest.NormBinData) sexEstimate {
+	xSizes := make([]float64, 0, 16384)
+	ySizes := make([]float64, 0, 16384)
+
+	for refBlock, nBin := range d.Bins {
+		if refBlock.Name == "X" && nBin.Size > float64(0) {
+			xSizes = append(xSizes, nBin.Size)
+		}
+		if refBlock.Name == "Y" && nBin.Size > float64(0) {
+			ySizes = append(ySizes, nBin.Size)
+		}
+	}
+
+	var (
+		xMedian float64
+		yMedian float64
+		gender  string
+	)
+
+	sort.Slice(xSizes, func(i, j int) bool { return xSizes[i] < xSizes[j] })
+	sort.Slice(ySizes, func(i, j int) bool { return ySizes[i] < ySizes[j] })
+
+	// Assuming ploidy of 2
+	if len(xSizes) > 0 {
+		xMedian = float64(2) * xSizes[int(float64(len(xSizes))/2)]
+	}
+	if len(ySizes) > 0 {
+		yMedian = float64(2) * ySizes[int(float64(len(ySizes))/2)]
+	}
+
+	xCopy := uint8(binest.Round(xMedian, 0.5, 0))
+	yCopy := uint8(binest.Round(yMedian, 0.5, 0))
+
+	if xCopy == 2 && yCopy == 0 {
+		gender = "female"
+	} else if xCopy == 1 && yCopy == 1 {
+		gender = "male"
+	} else {
+		gender = "unknown"
+	}
+
+	return sexEstimate{
+		gender:  gender,
+		xCopy:   xCopy,
+		yCopy:   yCopy,
+		xMedian: xMedian,
+		yMedian: yMedian,
+	}
+}
+
+func writeResults(results <-chan sexEstimate, fin chan<- bool, outStream *os.File) {
+	for result := range results {
+		fmt.Fprintln(outStream, result)
+	}
+	fin <- true
+}
+
+// sexEstimate holds the result of the sex estimate of the sample
+type sexEstimate struct {
+	sampleName string
+	gender     string
+	xCopy      uint8
+	yCopy      uint8
+	xMedian    float64
+	yMedian    float64
+}
+
+// String implements the Stringer interface for sexEstimate
+func (s sexEstimate) String() string {
+	var out string
+	for i := 0; i < int(s.xCopy); i++ {
+		out += "X"
+	}
+	for i := 0; i < int(s.yCopy); i++ {
+		out += "Y"
+	}
+
+	return fmt.Sprintf("%s\t%s\t%s\t%.10f\t%.10f",
+		s.sampleName, s.gender, out, s.xMedian, s.yMedian)
+}
