1bd54bd6e4e7239a5826c35233dcc5c6acacfb6b
Rajeeva Musunuri <rmusunuri@nygenome.org>
2017-04-20 12:14:20 -0400

    check of indexes more explicitly

--- sex/sex.go
+++ sex/sex.go
@@ -71,13 +71,17 @@ func EstimateSex(bampaths <-chan string, estimates chan<- sexEstimate, procs int
 			binest.CheckError(err)
 			defer bamFh.Close()
 
-			bamIdxFh, err := os.Open(fmt.Sprintf("%s.bai", b))
-			if err == os.ErrNotExist {
+			var bamIdxFh *os.File
+			if _, err = os.Stat(fmt.Sprintf("%s.bai", b)); err == nil {
+				bamIdxFh, err = os.Open(fmt.Sprintf("%s.bai", b))
+				binest.CheckError(err)
+			} else if _, err = os.Stat(b[:len(b)-4] + ".bai"); err == nil {
 				bamIdxFh, err = os.Open(b[:len(b)-4] + ".bai")
 				binest.CheckError(err)
 			} else {
-				binest.CheckError(err)
+				panic(fmt.Errorf("No BAM index found for %s\n", b))
 			}
+
 			defer bamIdxFh.Close()
 
 			bamRdr, err := bam.NewReader(bamFh, 2)
