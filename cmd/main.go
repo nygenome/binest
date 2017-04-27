c23cb4fab285e8169e2e1c37a50d85921ca5d966
Rajeeva Musunuri <rmusunuri@nygenome.org>
2017-04-27 19:55:49 -0400

    do checks before routing to subcommands

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
 
@@ -12,99 +11,59 @@ import (
 func main() {
 
 	var (
-		desc    = "Estimate density, copy number and sex from BAI/TBI index bins"
+		desc    = "Estimate copy number, density and sex from BAI/TBI index bins."
 		app     = kingpin.New("binest", desc).Version(fmt.Sprintf("binest v%s", binest.Version))
-		faiPath = app.Flag("fai", "path to reference FAI index").ExistingFile()
-		nProcs  = app.Flag("procs", "number of cores to use").Default("1").Uint()
+		faiPath = app.Flag("fai", "path to reference FAI index.").ExistingFile()
+		nProcs  = app.Flag("procs", "number of cores to use.").Default("1").Uint()
 
-		copyCmd    = app.Command("copy", "Estimate per chromosome copy number with BAI index")
-		copyIdxs   = copyCmd.Arg("index", "path to one or more BAI indexes").ExistingFiles()
-		copyPloidy = copyCmd.Flag("ploidy", "ploidy to use for copy number estimate").Default("2").Uint()
+		copyCmd    = app.Command("copy", "Estimate per chromosome copy number with BAI index.")
+		copyIdxs   = copyCmd.Arg("index", "path to one or more BAI indexes.").ExistingFiles()
+		copyPloidy = copyCmd.Flag("ploidy", "ploidy to use for copy number estimate.").Default("2").Uint()
 
-		densCmd  = app.Command("density", "Compute density across 16kb bins with BAI/TBI index")
-		densIdxs = densCmd.Arg("index", "path to one or more BAI/TBI indexes").ExistingFiles()
-		densRaw  = densCmd.Flag("raw", "output raw sizes without normalization").Default("false").Bool()
+		densCmd  = app.Command("density", "Compute density across 16kb bins with BAI/TBI index.")
+		densIdxs = densCmd.Arg("index", "path to one or more BAI/TBI indexes.").ExistingFiles()
+		densRaw  = densCmd.Flag("raw", "output raw sizes without normalization.").Default("false").Bool()
 
-		sexCmd    = app.Command("sex", "Estimate sex genotype of a sample with BAI index")
-		sexIdxs   = sexCmd.Arg("index", "path to one or more BAI indexes").ExistingFiles()
-		sexPloidy = sexCmd.Flag("ploidy", "ploidy to use for copy number estimate").Default("2").Uint()
+		sexCmd    = app.Command("sex", "Estimate sex genotype of a sample with BAI index.")
+		sexIdxs   = sexCmd.Arg("index", "path to one or more BAI indexes.").ExistingFiles()
+		sexPloidy = sexCmd.Flag("ploidy", "ploidy to use for copy number estimate.").Default("2").Uint()
 	)
 
 	app.HelpFlag.Short('h')
 	app.VersionFlag.Short('v')
 
-	switch kingpin.MustParse(app.Parse(os.Args[1:])) {
+	cmd, err := app.Parse(os.Args[1:])
+
+	var (
+		idxs []string
+		ok   bool
+	)
+
+	switch cmd {
+	case "copy", "density", "sex":
+		if *faiPath == "" {
+			fmt.Fprintf(os.Stderr, "No reference FAI index provided!\n\n")
+			app.Usage(os.Args[1:])
+			os.Exit(1)
+		}
+		idxs, ok = readIndexes([3][]string{*copyIdxs, *densIdxs, *sexIdxs})
+		if !ok {
+			fmt.Fprintf(os.Stderr, "No indexes provided!\n\n")
+			app.Usage(os.Args[1:])
+			os.Exit(1)
+		}
+	}
+
+	switch kingpin.MustParse(cmd, err) {
 	case "copy":
-		if *faiPath == "" {
-			kingpin.FatalUsage("No reference FAI index provided!")
-		}
-
-		indexes, ok := readIndexes(*copyIdxs)
-		if !ok {
-			kingpin.FatalUsage("No indexes provided to estimate copy number!")
-		}
-
-		runCopy(indexes, *faiPath, *copyPloidy, *nProcs)
-
+		runCopy(idxs, *faiPath, *copyPloidy, *nProcs)
 	case "density":
-		if *faiPath == "" {
-			kingpin.FatalUsage("No reference FAI index provided!")
-		}
-
-		indexes, ok := readIndexes(*densIdxs)
-		if !ok {
-			kingpin.FatalUsage("No indexes provided to get densities!")
-		}
-
-		runDensity(indexes, *faiPath, *densRaw, *nProcs)
-
+		runDensity(idxs, *faiPath, *densRaw, *nProcs)
 	case "sex":
-		if *faiPath == "" {
-			kingpin.FatalUsage("No reference FAI index provided!")
-		}
-
-		indexes, ok := readIndexes(*sexIdxs)
-		if !ok {
-			kingpin.FatalUsage("No indexes provided to estimate sex!")
-		}
-
-		runSex(indexes, *faiPath, *sexPloidy, *nProcs)
-
+		runSex(idxs, *faiPath, *sexPloidy, *nProcs)
 	case "help":
 		fmt.Fprintln(os.Stderr, app.Help)
 		os.Exit(1)
 	}
 
-}
-
-// readIndexes merges all indexes if any, from both cli arguments and stdin
-func readIndexes(args []string) ([]string, bool) {
-	indexes := make([]string, 0, 100)
-	indexes = append(indexes, args...)
-
-	if hasStdin() {
-		bufScanner := bufio.NewScanner(os.Stdin)
-		for bufScanner.Scan() {
-			indexes = append(indexes, bufScanner.Text())
-		}
-		if err := bufScanner.Err(); err != nil {
-			fmt.Fprintln(os.Stderr, "Error reading data from stdin")
-			panic(err)
-		}
-	}
-
-	return indexes, len(indexes) > 0
-}
-
-// hasStdin checks if process can read from /dev/stdin
-func hasStdin() bool {
-	stat, err := os.Stdin.Stat()
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error checking for data from stdin")
-		panic(err)
-	}
-	if stat.Mode()&os.ModeCharDevice == 0 {
-		return true
-	}
-	return false
 }
