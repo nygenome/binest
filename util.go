2f607485f4ec9110d37b0c6de9ebd120a6cabfb4
Rajeeva Musunuri <rmusunuri@nygenome.org>
2018-08-23 12:08:25 -0400

    chore: remove unused functions

--- util.go
+++ util.go
@@ -50,35 +50,6 @@ func medianI64(arr []int64) (median float64) {
 	return median
 }
 
-// medianF64 returns the median from []float64.
-// recursively finds a non-zero median if possible.
-//func medianF64(arr []float64) (median float64) {
-//	if len(arr) <= 2 {
-//		return meanF64(arr)
-//	}
-//
-//	lessFunc := func(i, j int) bool { return arr[i] < arr[j] }
-//	if !sort.SliceIsSorted(arr, lessFunc) {
-//		sort.Slice(arr, lessFunc)
-//	}
-//
-//	arrLen := len(arr)
-//	if arrLen%2 == 0 {
-//		median = (arr[arrLen/2-1] + arr[arrLen/2+1]) / float64(2)
-//	} else {
-//		median = arr[arrLen/2]
-//	}
-//
-//	if median == 0 {
-//		currIdx := arrLen / 2
-//		for ; currIdx < arrLen && arr[currIdx] == 0; currIdx++ {
-//		}
-//		return medianF64(arr[currIdx:])
-//	}
-//
-//	return median
-//}
-
 // meanI64 returns the mean from []int64.
 func meanI64(arr []int64) (mean float64) {
 	var sum int64
@@ -88,16 +59,6 @@ func meanI64(arr []int64) (mean float64) {
 	mean = float64(sum) / float64(len(arr))
 	return mean
 }
-
-// meanF64 returns the mean from []float64.
-//func meanF64(arr []float64) (mean float64) {
-//	var sum float64
-//	for _, val := range arr {
-//		sum += val
-//	}
-//	mean = sum / float64(len(arr))
-//	return mean
-//}
 
 // stripKnownSuffixes strips known index suffixes to get sample name
 func stripKnownSuffixes(path string) string {
